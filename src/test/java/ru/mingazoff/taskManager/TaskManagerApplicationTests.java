a084c87e07a686c381c9a173a5125a45befe7be6
Renat Mingazov <mingazofff@gmail.com>
2024-11-20 20:56:27 +0300

    Spring initializr empty project

---
+++ src/test/java/ru/mingazoff/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mingazoff.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
