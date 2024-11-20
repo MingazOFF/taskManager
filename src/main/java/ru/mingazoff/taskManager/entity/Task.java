184504d1dc91ee1818ddb055e2533e3d4e834a95
Renat Mingazov <mingazofff@gmail.com>
2024-11-20 21:06:43 +0300

    add entity Task and DB settings

---
+++ src/main/java/ru/mingazoff/taskManager/entity/Task.java
@@ -0,0 +1,22 @@
+package ru.mingazoff.taskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title", length = 100)
+    private String title;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "user_id")
+    private int userId;
+}
